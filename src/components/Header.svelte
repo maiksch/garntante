82210a01e18db696687bb33c8f83c918e9dbd10d
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2020-03-05 20:54:23 +0100

    Added header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,163 @@
+<script>
+  export let segment;
+</script>
+
+<style>
+  .main-nav ul {
+    background: #d2615f;
+  }
+
+  @media (min-width: 52em) {
+    .main-nav ul {
+      justify-content: flex-start;
+      border: 1px solid transparent;
+      border-radius: 10px 10px 0 0;
+    }
+    .main-nav ul .active a {
+      background-color: white;
+      color: #d2615f;
+    }
+    .main-nav ul .active a:hover {
+      background-color: white;
+      color: #d2615f;
+    }
+  }
+  .main-nav ul li {
+    border-top: 1px solid rgba(255, 255, 255, 0.3);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  @media (min-width: 52em) {
+    .main-nav ul li {
+      flex: 1 1 0;
+      border-right: 1px solid rgba(255, 255, 255, 0.1);
+      border-left: 1px solid rgba(0, 0, 0, 0.1);
+      border-top: none;
+      border-bottom: none;
+    }
+    .main-nav ul li.dropdown-icon {
+      display: none;
+    }
+  }
+  .main-nav ul li a {
+    background-color: #d2615f;
+    font-family: "Roboto Slab", serif;
+    color: white;
+    text-align: center;
+    display: block;
+    padding: 10px;
+    font-size: 0.9em;
+  }
+  @media (min-width: 52em) {
+    .main-nav ul li a {
+      line-height: 25px;
+    }
+  }
+  .main-nav ul li a:hover {
+    color: white;
+    text-decoration: none;
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+  .main-nav ul li:first-child {
+    border-top: none;
+    border-bottom: 2px solid white;
+  }
+  .main-nav ul li:nth-child(2) {
+    border-top: none;
+  }
+  @media (min-width: 52em) {
+    .main-nav ul li:nth-child(2) {
+      border-left: none;
+    }
+    .main-nav ul li:nth-child(2) a {
+      border-radius: 10px 0 0 0;
+    }
+  }
+  .main-nav ul li:not(:first-child) {
+    display: none;
+  }
+  @media (min-width: 52em) {
+    .main-nav ul li:not(:first-child) {
+      display: block;
+    }
+  }
+  .main-nav ul li:last-child {
+    border-bottom: none;
+  }
+  @media (min-width: 52em) {
+    .main-nav ul li:last-child {
+      border-right: none;
+    }
+    .main-nav ul li:last-child a {
+      border-radius: 0 10px 0 0;
+    }
+  }
+  .main-nav ul li.show-mobile-menu {
+    display: block;
+  }
+
+  nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  .brand-logo {
+    padding: 15px 0;
+    display: flex;
+  }
+
+  .secondary-nav {
+    display: none;
+  }
+  .secondary-nav ul {
+    padding: 0.5em 0;
+  }
+  .secondary-nav ul a {
+    padding: 5px;
+    color: grey;
+  }
+  @media (min-width: 40em) {
+    .secondary-nav {
+      display: block;
+    }
+  }
+</style>
+
+<header class="container">
+  <div>
+    <div class="flex justify-between">
+      <a href="/">
+        <img class="brand-logo" alt="Garntante Logo" src="assets/logo.png" />
+      </a>
+
+      <nav class="secondary-nav">
+        <ul class="menu">
+          <li>
+            <a href="/impressum">Impressum</a>
+          </li>
+          <li>
+            <a href="/kontakt">Kontakt</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <nav class="main-nav">
+      <ul id="main-nav" class="menu">
+        <li class="dropdown-icon">
+          <a href="javascript:void(0);">Menu ☰</a>
+        </li>
+        <li class:active={segment === 'unsere-wolle'}>
+          <a href="/unsere-wolle">Über unsere Wolle</a>
+        </li>
+        <li class:active={segment === 'stricktreff-und-kurse'}>
+          <a href="/stricktreff-und-kurse">Strickkurse</a>
+        </li>
+        <li class:active={segment === 'wissen'}>
+          <a href="/wissen">Wissenswertes</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
