eef8da46551e10a834aad23540a9d6b9cbd9758c
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2020-03-07 12:52:09 +0100

    Added material design icon library and icon component

---
+++ src/components/Icon.svelte
@@ -0,0 +1,39 @@
+<script>
+  // import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+  // import { faGem } from "@fortawesome/free-regular-svg-icons";
+
+  import {
+    mdiDiamondStone,
+    mdiCommentAccountOutline,
+    mdiCoffeeOutline
+  } from "@mdi/js";
+
+  export let icon;
+  export let size = 1;
+
+  const dict = {
+    diamond: mdiDiamondStone,
+    comment: mdiCommentAccountOutline,
+    coffee: mdiCoffeeOutline
+  };
+
+  const path = dict[icon];
+  const fontSize = size * 24;
+</script>
+
+<style>
+  svg {
+    fill: currentColor;
+    width: 1em;
+    display: inline-block;
+    font-size: inherit;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+</style>
+
+{#if path}
+  <svg viewBox="0 0 24 24" style="font-size: {fontSize}px">
+    <path d={path} />
+  </svg>
+{/if}
