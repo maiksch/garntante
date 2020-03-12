ef85a7603916ba14a28c9daa34d29dd921d5f685
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2020-03-12 18:48:10 +0100

    Added strickkurse

---
+++ src/routes/home/Strickkurs.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let src = "";
+  export let title = "";
+</script>
+
+<style lang="scss">
+  .frontpage-event {
+    width: auto;
+  }
+
+  article {
+    position: relative;
+  }
+
+  img {
+    width: 100%;
+    height: 100px;
+    object-fit: cover;
+  }
+
+  .title {
+    position: absolute;
+    color: white;
+    top: 50%;
+    margin-top: -18px;
+    background-color: rgba(210, 97, 95, 0.6);
+    padding: 0 10px 5px 10px;
+  }
+
+  @media (min-width: 52em) {
+    .frontpage-event {
+      width: 50%;
+      float: left;
+      padding-left: 0.46296%;
+      padding-right: 0.46296%;
+    }
+  }
+
+  @media (min-width: 64em) {
+    .frontpage-event {
+      width: 33.33333%;
+      float: left;
+      padding-left: 0.46296%;
+      padding-right: 0.46296%;
+    }
+  }
+</style>
+
+<div class="frontpage-event">
+  <article>
+    <a href="/strickkurse">
+      <img {src} alt={title} />
+      <div class="title">
+        <b>{title}</b>
+      </div>
+    </a>
+  </article>
+</div>
--- src/routes/home/index.svelte
+++ src/routes/home/index.svelte
@@ -2,8 +2,30 @@
   import Icon from "../../components/Icon.svelte";
   import Banner from "../../components/Banner.svelte";
   import Qualitaet from "./Qualitaet.svelte";
+  import Strickkurs from "./Strickkurs.svelte";
 
-  let strickkurse = [];
+  let strickkurse = [
+    {
+      src: "/assets/strickkurse/stricktreff.jpg",
+      title: "Stricktreff"
+    },
+    {
+      src: "/assets/strickkurse/einzelstunde.jpg",
+      title: "Einzelstunde"
+    },
+    {
+      src: "/assets/strickkurse/anfaenger.jpg",
+      title: "Anfängerkurs: Stricken"
+    },
+    {
+      src: "/assets/strickkurse/anfaenger-haekeln.jpg",
+      title: "Anfängerkurs: Häkeln"
+    },
+    {
+      src: "/assets/strickkurse/toe-up.jpg",
+      title: "Toe-up Sockenstrickkurs"
+    }
+  ];
 </script>
 
 <style>
@@ -22,6 +44,39 @@
       max-width: inherit;
     }
   }
+
+  .frontpage-events-container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 333px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 52em) {
+    .frontpage-events-container {
+      width: 678px;
+    }
+  }
+
+  @media (min-width: 64em) {
+    .frontpage-events-container {
+      width: 100%;
+    }
+  }
+
+  .frontpage-heading h1 {
+    padding: 10px 15px 15px 15px;
+    margin: 0;
+    max-width: none;
+  }
+
+  .frontpage-heading h1 span {
+    border: 2px solid #d2615f;
+    border-right: 0;
+    border-left: 0;
+    border-top: 0;
+    font-size: 50px;
+  }
 </style>
 
 <Banner
@@ -29,7 +84,6 @@
   title="Deine Wollgeschäfte in Hamburg" />
 
 <section class="qualities-container">
-
   <Qualitaet icon="diamond" title="Hohe Qualität">
     Bei uns findest du traumhafte Garne und hochwertiges Strickzubehör. Unser
     Angebot findest du
@@ -48,34 +102,18 @@
     Ob bei der Auswahl deiner Wolle oder bei unserem Stricktreff mit
     Gleichgesinnten, fühle dich bei uns ganz wie Zuhause.
   </Qualitaet>
-
 </section>
 
-<!-- <section class="frontpage-section">
+<section>
+  <div class="frontpage-heading center hreset">
+    <h1 class="hreset">
+      <span>Gemeinsam Stricken</span>
+    </h1>
+  </div>
 
-    <div class="frontpage-heading center hreset">
-      <h1 class="m0 hreset">
-        <span>Gemeinsam Stricken</span>
-      </h1>
-    </div>
-
-    <div class="frontpage-events-container">
-      {#each strickkurse as strickkurs}
-        <div class="frontpage-event">
-          <article>
-            <a href={strickkurs.link}>
-              <img
-                class="frontpage-event-image"
-                src={strickkurs.imageSrc}
-                alt={strickkurs.imageAlt} />
-              <div class="frontpage-event-title">
-                <b>{strickkurs.title}</b>
-              </div>
-            </a>
-          </article>
-        </div>
-      {/each}
-    </div>
-
-  </section> -->
-<!-- </div> -->
+  <div class="frontpage-events-container">
+    {#each strickkurse as strickkurs}
+      <Strickkurs src={strickkurs.src} title={strickkurs.title} />
+    {/each}
+  </div>
+</section>
